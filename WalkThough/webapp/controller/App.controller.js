1151ff8cab3497c468b9c164223c50ecb7584f87
xiaoboybaby <308393896@qq.com>
2016-11-02 16:39:39 +0800

    1

---
+++ WalkThough/webapp/controller/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    	"sap/ui/core/mvc/Controller",
+    	"sap/m/MessageToast"
+    ], function (Controller) {
+    	"use strict";
+     
+    	return Controller.extend("sap.ui.demo.wt.controller.App", {
+     
+    		
+    	});
+     
+    });
